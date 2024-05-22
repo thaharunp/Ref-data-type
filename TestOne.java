f752e2f7ba329f567bded274bcb22c517b99049d
sadhakunps <sadaqunps@gmail.com>
2024-05-22 17:43:28 +0530

    txt

---
+++ TestOne.java
@@ -0,0 +1,11 @@
+class Test{
+    public static void main(String []args){
+        
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[0]+args[1]);
+        //System.out.println(args["Rahul"+"gandhi"]);
+        System.out.println(args[20+10]);
+    }
+}
